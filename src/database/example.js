b8fb72da9c70ddf8788c506560fb626acd83ea7b
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-13 23:23:15 -0400

    add books json

--- src/database/example.js
+++ src/database/example.js
@@ -1,5 +1,5 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const fs = require('fs')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.USER_KEY
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -10,11 +10,32 @@ async function run() {
     await client.connect();
     const db = client.db('lezama')
     const poems = db.collection('poems');
-    console.dir(
-        await poems.updateMany()
-    )
+    const all = await poems.find().toArray()
+
+    const books = {
+        
+    }
+
+    const result = []
+
+    all.forEach(poem=>{
+        books[poem.book] = (books[poem.book]?.concat([poem._id]) ||[poem._id])
+    })
+
+
+    fs.writeFileSync('./books.json',JSON.stringify(books))
+    
+
   } finally {
     await client.close();
   }
 }
 run().catch(console.dir);
+
+async function updateBookTitleInPoems(collection ,currentBookTitle, updateTitle){
+    await collection.updateMany({
+        book: currentBookTitle
+    },{
+        $set: {book: updateTitle}
+    })
+}
